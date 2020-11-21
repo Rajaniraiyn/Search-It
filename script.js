725ee1758c5bf6c9e217a1c6db0d1518b0b732e3
Rajaniraiyn <72294760+Rajaniraiyn@users.noreply.github.com>
2020-11-21 12:05:52 +0530

    improved and removed unwanted JavaScript

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-function myS1(){
-  if(0===document.getElementById("S1").value){
-    var a=document.getElementById("noqry");
-    a.className="show";
-    setTimeout(function(){
-      a.className=a.className.replace("show","")},3E3)}
-  else location.href="https://rajaniraiyn.github.io/Search-It/?p="+document.getElementById("S1").value}
-function myS2(){
-  if(0===document.getElementById("S2").value){
-    var a=document.getElementById("noqry");
-    a.className="show";
-    setTimeout(function(){
-      a.className=a.className.replace("show","")},3E3)}
-  else location.href="https://rajaniraiyn.github.io/Search-It/?c="+document.getElementById("S2").value}
-function offLine(){
-  var a=document.getElementById("ofline");a.className="show";
-  setTimeout(function(){
-    a.className=a.className.replace("show","")},3E3)}
-setInterval(function(){
-  0==navigator.onLine&&offLine()},1E3);
+  function search(e) {
+    if (0 === document.getElementById(e).value) {
+      var a = document.getElementById("noqry");
+      a.className = "show",
+        setTimeout(function () {
+        a.className = t.className.replace("show", "")
+      }  , 3e3)
+    }
+    else 
+      "s1" == e
+        ? location.href = "https://rajaniraiyn.github.io/Search-It/?p=" + document.getElementById(e).value
+      : "s2" == e
+        ? location.href = "https://rajaniraiyn.github.io/Search-It/?c=" + document.getElementById(e).value
+      : console.log()
+      }
+  function offLine() {
+    var e = document.getElementById("ofline");
+    a.className = "show",
+      setTimeout(function () {
+      a.className = a.className.replace("show", "")
+    }  , 3e3)
+  }
+  setInterval(function () {
+    0 == navigator.onLine && offLine()
+  }  , 1e3);
