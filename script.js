34e54d4691f741ec4332dddcfcd85299450ab4c1
Rajaniraiyn <72294760+Rajaniraiyn@users.noreply.github.com>
2020-11-24 12:52:33 +0530

    added swiching mode functionality

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-  function search(e) {
+function search(e) {
     if (0 == document.getElementById(e).value) {
-      var n = document.getElementById("noqry");
-      n.className = "show",
+        var n = document.getElementById("noqry");
+        n.className = "show",
         setTimeout(function () {
-        n.className = n.className.replace("show", "")
-      }  , 3e3)
-    }
-    else 
-      "s1" == e
-        ? location.href = "https://rajaniraiyn.github.io/Search-It/?p=" + document.getElementById(e).value
-      : "s2" == e
-        ? location.href = "https://rajaniraiyn.github.io/Search-It/?c=" + document.getElementById(e).value
-      : console.log()
-      }
-  function offLine() {
+            n.className = n.className.replace("show", "")
+        }, 3e3)
+    } else 
+        "s1" == e
+            ? location.href = "https://rajaniraiyn.github.io/Search-It/?p=" + document.getElementById(e).value
+            : "s2" == e
+                ? location.href = "https://rajaniraiyn.github.io/Search-It/?c=" + document.getElementById(e).value
+                : console.log()
+        }
+function offLine() {
     var o = document.getElementById("ofline");
     o.className = "show",
-      setTimeout(function () {
-      o.className = o.className.replace("show", "")
-    }  , 3e3)
-  }
-  setInterval(function () {
+    setTimeout(function () {
+        o.className = o.className.replace("show", "")
+    }, 3e3)
+}
+setInterval(function () {
     0 == navigator.onLine && offLine()
-  }  , 1e3);
+}, 1e3);
+function btn() {
+    var a = document.getElementById("e1"),
+        b = document.getElementById("e2"),
+        c = document.getElementById("e3");
+    "none" === a.style.display
+        ? (a.style.display = "block",
+           b.style.display = "none",
+           c.innerHTML = "Switch to Complete Mode")
+        : (a.style.display = "none",
+           b.style.display = "block",
+           c.innerHTML = "Switch to Private Mode")
+};
